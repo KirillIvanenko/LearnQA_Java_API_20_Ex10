d0abee69f54c6c34173504c979523b5d6ea74722
Kirill Ivanenko <kv.ivanenko@mail.ru>
2024-03-29 20:04:38 +0300

    First JUnit test

---
+++ src/test/java/JunitPhraseTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JunitPhraseTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"HelloSomeBody", "HelloSomeBodyToday"}) // HelloSomeBody are 13 symbols and HelloSomeBodyToday are 17 symbols
+
+    public void jUnitPhraseTest(String phrase) {
+        String expectedName = (phrase.length() > 15) ? phrase : "";
+        assertEquals("HelloSomeBodyToday",expectedName, "Phrase more shortly when 15 characters");
+    }
+}
